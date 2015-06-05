7c695998069c5b09d1da5ec6652005a44c14050b
Martin Oehler <martin.sven.oehler@gmail.com>
2015-06-04 21:37:46 -0700

    started implementation of pitch advise

--- thor_mang_com_adjustment/src/thor_mang_com_adjustment/com_adjustment_widget.py
+++ thor_mang_com_adjustment/src/thor_mang_com_adjustment/com_adjustment_widget.py
@@ -17,6 +17,7 @@ from python_qt_binding.QtGui import QWidget, QHBoxLayout, QVBoxLayout, QPushButt
 
 import PyKDL
 
+
 class CoMAdjustmentDialog(Plugin):
 
     def __init__(self, context):
@@ -30,6 +31,7 @@ class CoMAdjustmentDialog(Plugin):
 
     def shutdown_plugin(self):
         self._widget.shutdown_plugin()
+
 
 class CoMAdjustmentWidget(QObject):
 
@@ -53,6 +55,7 @@ class CoMAdjustmentWidget(QObject):
         self.com_adjustment_widget.slider_AnklePitch.valueChanged[int].connect(self._handle_slider_ankle_changed)
         self.com_adjustment_widget.spin_HipPitch.valueChanged[float].connect(self._handle_spin_hip_changed)
         self.com_adjustment_widget.spin_AnklePitch.valueChanged[float].connect(self._handle_spin_ankle_changed)
+        self.com_adjustment_widget.lineEdit_TargetIMUPitch.textChanged
         
         # end widget
         widget.setLayout(vbox)
@@ -64,7 +67,8 @@ class CoMAdjustmentWidget(QObject):
         
         # init publisher
         self.pitch_values_pub = rospy.Publisher("/thor_mang/step_controller/pitch_offset", std_msgs.msg.Float64MultiArray, queue_size=1)
-
+        self.got_init_values = False
+        self.target_imu = 0.0
 
     def shutdown_plugin(self):
         print "Shutting down ..."
@@ -79,30 +83,36 @@ class CoMAdjustmentWidget(QObject):
       
         self.com_adjustment_widget.spin_CurrentHipPitch.setValue(current_values_msg.data[0])
         self.com_adjustment_widget.spin_CurrentAnklePitch.setValue(current_values_msg.data[1])
+        if not self.got_init_values:
+            self.got_init_values = True
+            self.com_adjustment_widget.slider_HipPitch.setValue(current_values_msg.data[0] * 100)
+            self.com_adjustment_widget.slider_AnklePitch.setValue(current_values_msg.data[1] * 100)
+            self.com_adjustment_widget.spin_HipPitch.setValue(current_values_msg.data[0])
+            self.com_adjustment_widget.spin_AnklePitch.setValue(current_values_msg.data[1])
 
     def _imu_pitch_callback(self, imu_msg):
         rotation = PyKDL.Rotation.Quaternion(imu_msg.orientation.x, imu_msg.orientation.y, imu_msg.orientation.z, imu_msg.orientation.w)
         [roll, pitch, yaw] = rotation.GetRPY()
         
-        pitch_str = "{0:2.4f}";
+        pitch_str = "{0:2.4f}"
 
         self.com_adjustment_widget.lineEdit_CurrentIMUPitch.setText(pitch_str.format(pitch))
         
     def _handle_send_pitch_clicked(self):
-        current_pitch_msg = std_msgs.msg.Float64MultiArray(data = [0] * 2)
+        current_pitch_msg = std_msgs.msg.Float64MultiArray(data=[0] * 2)
         current_pitch_msg.data[0] = self.com_adjustment_widget.spin_HipPitch.value()
         current_pitch_msg.data[1] = self.com_adjustment_widget.spin_AnklePitch.value()
         self.pitch_values_pub.publish(current_pitch_msg)
         
     def _handle_slider_hip_changed(self, value):
-        self.com_adjustment_widget.spin_HipPitch.setValue(value/100.0)
+        self.com_adjustment_widget.spin_HipPitch.setValue(value / 100.0)
         
     def _handle_slider_ankle_changed(self, value):
-        self.com_adjustment_widget.spin_AnklePitch.setValue(value/100.0)
+        self.com_adjustment_widget.spin_AnklePitch.setValue(value / 100.0)
         
     def _handle_spin_hip_changed(self, value):
         self.com_adjustment_widget.slider_HipPitch.blockSignals(True)
-        self.com_adjustment_widget.slider_HipPitch.setValue(value*100.0)
+        self.com_adjustment_widget.slider_HipPitch.setValue(value * 100.0)
         self.com_adjustment_widget.slider_HipPitch.blockSignals(False)
         
     def _handle_spin_ankle_changed(self, value):
